e8a5d3fbe5503b1341ab52f68058f1c3189f09fb
Vincent Nguyen <nguyenvincentvn@gmail.com>
2025-09-03 22:42:05 -0500

    Able to detect two gestures

--- handGesture.py
+++ handGesture.py
@@ -1,22 +1,37 @@
 import cv2
+import mediapipe as mp
+
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,600)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,500)
 
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+mp_hands = mp.solutions.hands
+hand = mp_hands.Hands()
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
+def findGesture(hand):
+    if hand.landmark[8].y > hand.landmark[6].y:
+        navigate("fist")
+    elif hand.landmark[8].y < hand.landmark[6].y:
+        navigate("flex")
+
+def navigate(gesture):
+    match gesture:
+        case "fist":
+            #replace prints to pyautogui to move mouse
+            print("fist")
+        case "flex":
+            print("flex")
 
 while True:
-    ret, frame = cam.read()
-
-    out.write(frame)
-
-    cv2.imshow('Camera', frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-    
+    success, frame = cam.read()
+    if success:
+        image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        result = hand.process(image)
+        if result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks:
+                findGesture(hand_landmarks)
+        cv2.imshow('Camera', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
 cam.release()
-out.release()
 cv2.destroyAllWindows()
